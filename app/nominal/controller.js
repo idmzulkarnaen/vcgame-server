0d0598cd71d8f2bb8b543ca5e542f376d7233615
idmzulkarnaen <kreatifku1995@gmail.com>
2021-10-26 21:15:51 +0700

    halaman nominal crud= DELETE

--- app/nominal/controller.js
+++ app/nominal/controller.js
@@ -79,7 +79,7 @@ module.exports = {
         { coinName, coinQuantity, price }
       );
 
-      req.flash("alertMessage", "Berhasil ubah kategori");
+      req.flash("alertMessage", "Berhasil ubah nominal");
       req.flash("alertStatus", "success");
 
       res.redirect("/nominal");
@@ -90,22 +90,22 @@ module.exports = {
     }
   },
 
-//   actionDelete: async (req, res) => {
-//     try {
-//       const { id } = req.params;
+  actionDelete: async (req, res) => {
+    try {
+      const { id } = req.params;
 
-//       await Category.findOneAndRemove({
-//         _id: id,
-//       });
+      await Nominal.findOneAndRemove({
+        _id: id,
+      });
 
-//       req.flash("alertMessage", "Berhasil hapus kategori");
-//       req.flash("alertStatus", "success");
+      req.flash("alertMessage", "Berhasil hapus nominal");
+      req.flash("alertStatus", "success");
 
-//       res.redirect("/category");
-//     } catch (err) {
-//       req.flash("alertMessage", `${err.message}`);
-//       req.flash("alertStatus", "danger");
-//       res.redirect("/category");
-//     }
-//   },
+      res.redirect("/nominal");
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/nominal");
+    }
+  },
 };
