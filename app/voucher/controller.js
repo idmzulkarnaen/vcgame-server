71ece82383c2214914d18b38f00945cf84b21f62
idmzulkarnaen <kreatifku1995@gmail.com>
2021-10-27 20:19:09 +0700

    halaman voucher crud= update saat gambar diganti walaupun berhasil gambar gak muncul

--- app/voucher/controller.js
+++ app/voucher/controller.js
@@ -102,4 +102,96 @@ module.exports = {
     }
   },
 
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req.params
+      const category = await Category.find()
+      const nominal = await Nominal.find()
+      const voucher = await Voucher.findOne({ _id: id })
+        .populate('category')
+        .populate('nominals')
+
+      res.render('admin/voucher/edit', {
+        voucher,
+        nominal,
+        category
+      })
+
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/voucher')
+    }
+  },
+
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params
+      const { name, category, nominals } = req.body
+
+      if(req.file){
+        let tmp_path= req.file.path;
+        let originaExt = req.file.originalname.split('.')[req.file.originalname.split('.').length - 1];
+        let filename = req.file.filename + '.' + originaExt;
+        let target_path = path.resolve(config.rootPath, `public/uploads/${filename}`)
+
+        const src = fs.createReadStream(tmp_path)
+        const dest = fs.createWriteStream(target_path)
+
+        src.pipe(dest)
+
+        src.on('end', async ()=>{
+          try {
+
+            const voucher = await Voucher.findOne({_id: id})
+
+            let currentImage = `${config.rootPath}/public/uploads/${voucher.thumbnial}`;
+            
+            if(fs.existsSync(currentImage)){
+              fs.unlinkSync(currentImage)
+            }
+
+            await Voucher.findOneAndUpdate({
+              _id : id
+            },{
+              name,
+              category,
+              nominals,
+              // thumbnial: filename
+            })
+            
+
+            req.flash('alertMessage', "Berhasil ubah voucher")
+            req.flash('alertStatus', "success")
+      
+            res.redirect('/voucher')
+            
+          } catch (err) {
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/voucher')
+          }
+        })
+      }else{
+        await Voucher.findOneAndUpdate({
+          _id : id
+        },{
+          name,
+          category,
+          nominals,
+        })
+        
+        req.flash('alertMessage', "Berhasil ubah voucher")
+        req.flash('alertStatus', "success")
+  
+        res.redirect('/voucher')
+      }
+
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/nominal')
+    }
+  },
+
 }
