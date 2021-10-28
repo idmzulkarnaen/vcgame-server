db85a2fc1d711b21a9492b1e3590ae4873562fd6
idmzulkarnaen <kreatifku1995@gmail.com>
2021-10-28 08:51:31 +0700

    halaman voucher crud= delete

--- app/voucher/controller.js
+++ app/voucher/controller.js
@@ -194,4 +194,30 @@ module.exports = {
     }
   },
 
+  actionDelete: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const voucher = await Voucher.findOneAndRemove({
+        _id: id
+      });
+
+      let currentImage = `${config.rootPath}/public/uploads/${voucher.thumbnial}`;
+      if(fs.existsSync(currentImage)){
+        fs.unlinkSync(currentImage)
+      }
+
+
+      req.flash('alertMessage', "Berhasil hapus voucher")
+      req.flash('alertStatus', "success")
+
+      res.redirect('/voucher')
+
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/voucher')
+    }
+  },
+
 }
